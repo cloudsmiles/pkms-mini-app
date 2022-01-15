f0dcd298e30cd5b69075e0fdacb4ecb52941295e
何文锋lovingCloud <cloudhewenfeng@gmail.com>
2022-01-15 17:16:11 +0800

    update

--- pages/list/list.js
+++ pages/list/list.js
@@ -4,7 +4,14 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    allList: [],
+    pairList: [],
+    inputVal: "",
+    inputShowed: false,
+    totalNum: 0,
+    page: 0,
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -37,7 +44,7 @@ Page({
       inputVal: e.detail.value
     });
   },
-  initData: function() {
+  initData: function () {
     getApp().globalData.pairList.forEach(function (item, index, array) {
       // Set default picture
       if (array[index].picture == "" || array[index].picture == undefined) {
@@ -45,24 +52,52 @@ Page({
       }
     })
     this.setData({
-      pairList: getApp().globalData.pairList
+      allList: getApp().globalData.pairList,
+      pairList: getApp().globalData.pairList.slice(0, 10),
+      totalNum: getApp().globalData.pairList.length,
+      page: 1,
     })
   },
-   getList: function(inputValue) {
+  binbottom:function(e){
+    if (this.data.pairList.length === this.data.totalNum) {
+      return 
+    } else{
+      //我们重复之前的数据
+      let startNum, lastNum
+      let that = this
+      that.data.page++
+      lastNum = that.data.page*10
+      startNum = (that.data.page-1)*10
+
+      console.log(startNum, lastNum);
+      this.data.pairList = this.data.pairList.concat(that.data.allList.slice(startNum, lastNum));
+      this.setData({
+       pairList: that.data.pairList,
+       page: that.data.page,
+      })
+    }
+  },
+  getList: function (inputValue) {
     // 根据输入值匹模糊匹配名称
     if (inputValue != "" && inputValue != undefined) {
       let newPairList = []
-       for (let pair of getApp().globalData.pairList) {
-         if (pair.name.indexOf(inputValue) != -1) {
-            newPairList.push(pair)
-         }
-       }
-       this.setData({
-         pairList: newPairList
-       })
+      for (let pair of getApp().globalData.pairList) {
+        if (pair.name.indexOf(inputValue) != -1) {
+          newPairList.push(pair)
+        }
+      }
+      this.setData({
+        allList: newPairList,
+        pairList: newPairList.slice(0, 10),
+        totalNum: newPairList.length,
+        page: 1,
+      })
     } else {
       this.setData({
-        pairList: getApp().globalData.pairList
+        allList: getApp().globalData.pairList,
+        pairList: getApp().globalData.pairList.slice(0, 10),
+        totalNum: getApp().globalData.pairList.length,
+        page: 1,
       })
     }
   }
